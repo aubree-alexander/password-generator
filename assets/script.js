7ee56e2bc98716fd9ac3b8f0177c002f14c8b6d6
Aubree Alexander <aubree.alexander@gmail.com>
2022-03-18 16:00:16 -0500

    update if statements

--- assets/script.js
+++ assets/script.js
@@ -2,66 +2,125 @@
 var generateBtn = document.querySelector("#generate");
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-var specialChar = ['!', '?', '%', '#', '@', '*', '^', '$', '&']
+var specialChar = ['!', '?', '%', '#', '@', '*', '^', '$', '&', '+', '-', '.', ':', ';']
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-var password = ""
+var randomPassword = ""
 
+//included in starter code
 
-
-var writePassword = function() {
-  var availableCharacters = []
-  var promptLength = prompt("How long would you like your password to be? Choose a number between 8 and 128.")
-
-  if (promptLength === "" || promptLength === null) {
-    alert("Please enter a valid number.")
-    return writePassword();
-  } else if (promptLength < 8 || promptLength > 128) {
-    alert("Please enter a number between 8 and 128.")
-    return writePassword()
-  }
-  
-
-  var promptUpper = confirm("Would you like your password to include uppercase letters? Click 'OK' for yes, and 'Cancel' for no.")
-    if (promptUpper) {
-      availableCharacters = availableCharacters.concat(upperCase)
-    }
-  var promptLower = confirm("Would you like your password to include lowercase letters? Click 'OK' for yes, and 'Cancel' for no.")
-    if (promptLower) {
-      availableCharacters = availableCharacters.concat(lowerCase)
-    }
-  var promptSpecialChar = confirm("Would you like your password to contain special characters? Click 'OK' for yes, and 'Cancel' for no.")
-    if (promptSpecialChar) {
-      availableCharacters = availableCharacters.concat(specialChar)
-    }
-  var promptNumbers = confirm("Would you like your password to contain numbers? Click 'OK' for yes, and 'Cancel' for no.")
-    if (promptNumbers) {
-      availableCharactrs = availableCharacters.concat(numbers)
-    }
-
-  var finalCharacters = promptUpper + promptLower + promptSpecialChar + promptNumbers
-
-  for (i = 0; i < promptLength; i++) {
-    var combineCharacters = function() {
-      var randomCharacter = [Math.floor(Math.random() * promptLength)]
-      var applicableCharacter = availableCharacters[randomCharacter]
-      return applicableCharacter
-    }
-  }
-  password = password + combineCharacters()
-
-  return password
-  
-}
-
-
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+// function for password generation
+
+function generatePassword() {
+  //length of password
+  var passwordLength = askPasswordLength();
+  //characters user chooses 
+  var characterPreferences = askCharacterPreferences();
+  //character arrary based on user's choices
+  var passwordCharacters = generatePasswordCharacters(characterPreferences, passwordLength)
+  //shuffling the array
+  shuffleArray(passwordCharacters)
+
+  return arrayToString(passwordCharacters)
+}
+
+//ask user for length of password
+
+function askPasswordLength() {
+  var passwordLength = prompt("How long would you like your password to be? Choose a number between 8 and 128.")
+
+  if (passwordLength === "" || passwordLength === null) {
+    alert("Please enter a valid number.")
+    return askPasswordLength();
+  } else if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please enter a number between 8 and 128.")
+    return askPasswordLength()
+  }
+  return passwordLength;
+}
+  
+
+//ask user what characters they want to include
+
+function askCharacterPreferences() {
+  var characterPreferences = []
+
+  var upperConfirm = confirm("Would you like your password to include uppercase letters? Click 'OK' for yes, and 'Cancel' for no.")
+    if (upperConfirm) {
+      characterPreferences.push(upperCase);
+    }
+
+  var lowerConfirm = confirm("Would you like your password to include lowercase letters? Click 'OK' for yes, and 'Cancel for no.")
+    if (lowerConfirm) {
+      characterPreferences.push(lowerCase);
+    }
+
+  var specialCharConfirm = confirm("Would you like your password to include special characters? Click 'OK' for yes, and 'Cancel' for no.")
+    if (specialCharConfirm) {
+      characterPreferences.push(specialChar);
+    }
+
+  var numberConfirm = confirm("Would you lke your password to include numbers? Click 'OK' for yes, and 'Cancel' for no.")
+    if (numberConfirm) {
+      characterPreferences.push(numbers);
+    }
+
+    return characterPreferences;
+}
+  
+function generatePasswordCharacters(charArray, passwordLength) {
+  passwordCharacters = []
+
+  while (passwordCharacters.length < passwordLength) {
+
+
+    for (var i = 0; i < charArray.length; i++) {
+
+     
+      if (passwordCharacters.length >= passwordLength) {
+        break;
+      } else {
+        charTypeArray = charArray[i];
+        
+        passwordCharacters.push(charTypeArray[Math.floor(Math.random() * charArray[i].length)]);
+      }
+    }
+  }
+  return passwordCharacters;
+}
+
+
+
+function shuffleArray(array) {
+  // shuffle 3 times
+  for (var i = 0; i < 3; i++) {
+    
+    for(var j = 0; j < array.length; j++){
+      var value1 = array[j];
+      var randomIndex = Math.floor(Math.random()*array.length); //random index in array
+
+      // switch value 1 and value 2
+      array[j] = array[randomIndex];
+      array[randomIndex] = value1;
+    }
+  }
+}
+
+function arrayToSTring(array) {
+  password = "";
+  // convert password array into a string
+  for (var i = 0; i < array.length; i++) {
+    password = password + array[i];
+  }
+  return password;
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
